a83d19025311f75a04e8425508d3255572de1ff6
jytli <yitao.li@mail.utoronto.ca>
2020-11-01 21:50:14 -0500

    added UUID to User classes

--- src/main/entities/User.java
+++ src/main/entities/User.java
@@ -1,11 +1,22 @@
 package main.entities;
 
+import java.util.UUID;
+
 public abstract class User {
-    private String uuid;
+    private String username;
     private String password;
+    private UUID id = UUID.randomUUID();
 
     public User (String email, String password) {
-        this.uuid = email;
+        this.username = email;
         this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UUID getId() {
+        return id;
     }
 }
