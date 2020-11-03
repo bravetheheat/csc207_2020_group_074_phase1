866cad57159db25816f127288f7acbe0108fd6e7
mazewen1 <kath.ma@mail.utoronto.ca>
2020-11-02 21:05:44 -0500

    Add the description of the Use Case class as well as the ContactsManager constructor.

--- src/main/usecases/ContactsManager.java
+++ src/main/usecases/ContactsManager.java
@@ -6,14 +6,29 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.UUID;
 
+/**
+ * The ContactsManager holds a dictionary of lists of contacts of users.
+ *
+ * @author Zewen Ma
+ * @version 2.0
+ * @since 2020-11-02
+ */
 public class ContactsManager {
 
     private Dictionary<UUID, ArrayList<User>> contactListsofUsers;
-    private ArrayList<User> contactList;
 
+    /**
+     * A user is added to the contact manager system with user id(UUID). The initial contact list of
+     * the user is empty because the user does not have any contact except the user himself/herself
+     * when the user account is initially created.
+     * @param user that added to the contact manager
+     */
     public ContactsManager(User user){
-        this.contactList = new ArrayList<User>();
+        ArrayList<User> contactList = new ArrayList<User>();
+        contactList.add(user);
+        contactListsofUsers.put(user.getId(), contactList);
     }
+
 
     public ArrayList<User> getContactList(User user) {
         return contactListsofUsers.get(user.getId());
