3891bbd3c53faefa32950566a31564ee5b3bb355
jytli <yitao.li@mail.utoronto.ca>
2020-11-08 14:25:05 -0500

    capitalized Speaker and Attendee attributes

--- src/main/entities/Attendee.java
+++ src/main/entities/Attendee.java
@@ -3,6 +3,6 @@ package main.entities;
 public class Attendee extends User{
 
     public Attendee (String email, String password) {
-        super(email, password, "attendee");
+        super(email, password, "Attendee");
     }
 }
