498818461bf7ef53d82d48db1caf6a958bbd457d
mazewen1 <kath.ma@mail.utoronto.ca>
2020-11-09 13:49:00 -0500

    Version 3.0: change all parameter in to UUID instead of User.

--- src/main/usecases/ContactsManager.java
+++ src/main/usecases/ContactsManager.java
@@ -9,7 +9,7 @@ import java.util.*;
  * The ContactsManager holds a dictionary of lists of contacts of users.
  *
  * @author Zewen Ma
- * @version 2.0
+ * @version 3.0
  * @since 2020-11-02
  */
 
@@ -21,63 +21,64 @@ public class ContactsManager {
      * A user is added to the contact manager system with user id(UUID). The initial contact list of
      * the user is empty because the user does not have any contact except the user himself/herself
      * when the user account is initially created.
-     * @param user that added to the contact manager
+     * @param userid that added to the contact manager
      */
-    public ContactsManager(User user){
+    public ContactsManager(UUID userid){
         ArrayList<UUID> contactList = new ArrayList<UUID>();
-        contactList.add(user.getId());
-        contactListsofUsers.put(user.getId(), contactList);
+        contactList.add(userid);
+        contactListsofUsers.put(userid, contactList);
     }
 
     /**
      * Add a friend to the user's contactList and add user to friend's contactList as well
-     * @param user in the contactsListofUsers
-     * @param friend that should be added to the contact list of the user
+     * @param userid in the contactsListofUsers
+     * @param friendid that should be added to the contact list of the user
      *
      * Precondition: A user can add friend to his/her contact list iff friend is a valid user.
      */
-    public void addUser(User user, User friend) {
-        ArrayList<UUID> friend1 = contactListsofUsers.get(user.getId());
-        friend1.add(friend.getId());
-        contactListsofUsers.put(user.getId(), friend1);
-        ArrayList<UUID> friend2 = contactListsofUsers.get(friend.getId());
-        friend2.add(user.getId());
-        contactListsofUsers.put(friend.getId(), friend2);
+    public void addUser(UUID userid, UUID friendid) {
+        ArrayList<UUID> friend1 = contactListsofUsers.get(userid);
+        friend1.add(friendid);
+        contactListsofUsers.put(userid, friend1);
+        ArrayList<UUID> friend2 = contactListsofUsers.get(friendid);
+        friend2.add(userid);
+        contactListsofUsers.put(friendid, friend2);
     }
 
     /**
      * Remove a friend from the user's contactList and remove user from the friend's contactList as well.
-     * @param user in the contactsListofUsers
-     * @param friend that should be removed from the contact list of the user
+     * @param userid in the contactsListofUsers
+     * @param friendid that should be removed from the contact list of the user
      *
      * Precondition: A user can remove friend from his/her contact list iff friend is a valid user and the friend
      *               is in the contactList of user.
      */
-    public void removeUser(User user, User friend){
-        ArrayList<UUID> friend1 = contactListsofUsers.get(user.getId());
-        friend1.remove(friend.getId());
-        contactListsofUsers.put(user.getId(), friend1);
-        ArrayList<UUID> friend2 = contactListsofUsers.get(friend.getId());
-        friend2.remove(user.getId());
-        contactListsofUsers.put(friend.getId(), friend2);
+    public void removeUser(UUID userid, UUID friendid){
+        ArrayList<UUID> friend1 = contactListsofUsers.get(userid);
+        friend1.remove(friendid);
+        contactListsofUsers.put(userid, friend1);
+        ArrayList<UUID> friend2 = contactListsofUsers.get(friendid);
+        friend2.remove(userid);
+        contactListsofUsers.put(friendid, friend2);
     }
 
     /**
      * Return a contact list of a user stored in contactListsofUsers
-     * @param user in the contactsListofUsers
+     * @param userid in the contactsListofUsers
      */
-    public ArrayList<UUID> getContactList(User user) {
-        return contactListsofUsers.get(user.getId());
+    public ArrayList<UUID> getContactList(UUID userid) {
+        return contactListsofUsers.get(userid);
     }
 
     /**
      * Return True iff a user can message to friend.
      * A user can send a message to friend iff friend is in the contact list of the user and user is also in
      * the contact list of friend.
-     * @param user in the contactsListofUsers
+     * @param userid in the contactsListofUsers
+     * @param friendid that needs to be checked permission
      */
-    public boolean checkPermission(User user, User friend){
-        ArrayList<UUID> friend1 = contactListsofUsers.get(user.getId());
-        return friend1.contains(friend.getId());
+    public boolean checkPermission(UUID userid, UUID friendid){
+        ArrayList<UUID> friend1 = contactListsofUsers.get(userid);
+        return friend1.contains(friendid);
     }
 }
