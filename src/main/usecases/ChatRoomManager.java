81be904c831767c926ef4d1ae3597ebb0944a5d7
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-10 20:19:13 +0800

    add some Javadoc

--- src/main/usecases/ChatRoomManager.java
+++ src/main/usecases/ChatRoomManager.java
@@ -8,7 +8,7 @@ import java.util.*;
  * <code>ChatRoomManager</code> adds messages to each user's chat rooms
  *
  * @author Steven Yuan, David Zhao
- * @version 1.1
+ * @version 1.2
  * @since 2020-11-03
  */
 public class ChatRoomManager {
@@ -22,7 +22,6 @@ public class ChatRoomManager {
     public ChatRoomManager() {
         this.chatRoomList = new HashMap<>();
         this.usersToChatRoom = new HashMap<>();
-
     }
 
     /**
@@ -39,6 +38,10 @@ public class ChatRoomManager {
         }
     }
 
+    /**
+     * Map each user to his/her <code>ChatRoom</code>s
+     * @param chatRoom a chat room that contains users' messages
+     */
     private void populateUsersToChatRoom(ChatRoom chatRoom) {
         for (UUID participant : chatRoom.getParticipants()) {
             if (!this.usersToChatRoom.containsKey(participant)) {
@@ -51,6 +54,10 @@ public class ChatRoomManager {
         }
     }
 
+    /**
+     * Create a <code>ChatRoom</code>
+     * @return ID of the <code>ChatRoom</code> created
+     */
     public UUID createChatRoom() {
         ChatRoom newChatRoom = new ChatRoom();
         UUID newChatRoomId = newChatRoom.getId();
@@ -58,6 +65,11 @@ public class ChatRoomManager {
         return newChatRoomId;
     }
 
+    /**
+     * Create a <code>ChatRoom</code> with a list of participants
+     * @param participants a list of <code>Users</code>
+     * @return ID of the <code>ChatRoom</code> created
+     */
     public UUID createChatRoom(List<UUID> participants) {
         ChatRoom newChatRoom = new ChatRoom(participants);
         UUID newChatRoomId = newChatRoom.getId();
@@ -65,24 +77,48 @@ public class ChatRoomManager {
         return newChatRoomId;
     }
 
+    /**
+     *
+     * @param chatRoomId
+     */
     public void deleteChatRoom(UUID chatRoomId) {
         this.chatRoomList.remove(chatRoomId);
     }
 
+    /**
+     *
+     * @param chatRoomId
+     * @return
+     */
     public ChatRoom fetchChatRoom(UUID chatRoomId) {
         return this.chatRoomList.get(chatRoomId);
     }
 
+    /**
+     *
+     * @param chatRoomId
+     * @param messageId
+     */
     public void addMessageToChatRoom(UUID chatRoomId, UUID messageId) {
         ChatRoom chatRoom = this.chatRoomList.get(chatRoomId);
         chatRoom.addMessage(messageId);
     }
 
+    /**
+     *
+     * @param chatRoomId
+     * @return
+     */
     public List<UUID> fetchMessagesFromChatRoom(UUID chatRoomId) {
         ChatRoom chatRoom = this.chatRoomList.get(chatRoomId);
         return chatRoom.getMessages();
     }
 
+    /**
+     *
+     * @param userId
+     * @return
+     */
     public List<UUID> fetchUserChatRooms(UUID userId) {
         return this.usersToChatRoom.get(userId);
     }
