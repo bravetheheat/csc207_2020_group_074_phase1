a141f8d5bbc7ed22c6de230559f9a74cc2a997de
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-11 11:59:54 +0800

    add class Javadoc

--- src/main/presenters/MessageScreen.java
+++ src/main/presenters/MessageScreen.java
@@ -1,4 +1,35 @@
 package main.presenters;
 
-public class MessageScreen {
+import main.controllers.ProgramController;
+
+/**
+ * Provide a list of chat rooms and let user create chat room with
+ * someone in the contact list
+ *
+ * @author Steven Yuan
+ * @version 1.0
+ * @since 2020-11-03
+ */
+public class MessageScreen extends Screen {
+
+    public MessageScreen(ProgramController programController) {
+        super(programController);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    public void viewChatRooms() {
+
+    }
+
+    public void goToChatRoomScreen() {
+
+    }
+
+    public void startChatRoomWith() {
+
+    }
 }
