625b98127f94668086ae87639d21085bf5cdfc27
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-12 19:50:53 +0800

    initial push

---
+++ src/main/controllers/OrganizerChatRoomScreenController.java
@@ -0,0 +1,10 @@
+package main.controllers;
+
+import java.util.UUID;
+
+public class OrganizerChatRoomScreenController extends ChatRoomScreenController {
+
+    public OrganizerChatRoomScreenController(ProgramController programController, UUID chatRoomId) {
+        super(programController, chatRoomId);
+    }
+}
