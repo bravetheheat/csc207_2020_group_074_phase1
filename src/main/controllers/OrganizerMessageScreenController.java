95f594c8ff843afbbc4528c75baad988c54e247f
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-12 22:46:33 +0800

    add sendMessageToAll()

--- src/main/controllers/OrganizerMessageScreenController.java
+++ src/main/controllers/OrganizerMessageScreenController.java
@@ -2,4 +2,11 @@ package main.controllers;
 
 public class OrganizerMessageScreenController extends MessageScreenController {
 
+    public OrganizerMessageScreenController(ProgramController programController) {
+        super(programController);
+    }
+
+    public void sendMessageToAll() {
+
+    }
 }
