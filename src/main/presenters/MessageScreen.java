6a47b3a9b760476af9933e84fad71dd9f653b0ef
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-12 17:33:35 +0800

    remove logic and add println() methods

--- src/main/presenters/MessageScreen.java
+++ src/main/presenters/MessageScreen.java
@@ -1,5 +1,6 @@
 package main.presenters;
 
+import main.controllers.MessageScreenController;
 import main.controllers.ProgramController;
 
 import java.util.HashMap;
@@ -18,56 +19,76 @@ import java.util.Arrays;
 public class MessageScreen extends Screen {
 
     private UUID myUserId;
+    private MessageScreenController messageScreenController;
 
     public MessageScreen(ProgramController programController) {
         super(programController);
-        RegisterScreen registerScreen = new RegisterScreen(programController);
-        myUserId = programController.getUsersManager().getIDFromUsername(registerScreen.username);
+        myUserId = programController.getAuthController().fetchLoggedInUser();
+        messageScreenController = new MessageScreenController();
     }
 
     @Override
     public void start() {
-        viewChatRooms();
+//        viewChatRooms();
+//        System.out.println();
+//        System.out.println("1. Select a chat room.");
+//        System.out.println("2. Create a new chat room.");
+//        int input = scanner.nextInt();
+//        if (input == 1) {
+//            System.out.println("Enter the name of the chat room:");
+//            String chatRoomNameInput = scanner.nextLine();
+//            goToChatRoomScreen(chatRoomNameInput);
+//        }
+//        else {
+//            startChatRoomWithFriend();
+//        }
+    }
+
+    public void messageScreenStart() {
         System.out.println();
         System.out.println("1. Select a chat room.");
         System.out.println("2. Create a new chat room.");
-        int input = scanner.nextInt();
-        if (input == 1) {
-            System.out.println("Enter the name of the chat room:");
-            String chatRoomNameInput = scanner.nextLine();
-            goToChatRoomScreen(chatRoomNameInput);
-        }
-        else {
-            startChatRoomWithFriend();
-        }
     }
 
-    public void startChatRoomWithFriend() {
-        // UUID newChatRoomId;
-        System.out.println("Enter the username of the person you want to chat with:");
-        String friendUsername = scanner.nextLine();
-        UUID friendUserId = programController.getUsersManager().
-                getIDFromUsername(friendUsername);
-        System.out.println("Create a name for the chat room:");
-        while (true) {
-            String chatRoomNameInput = scanner.nextLine();
-            if (!programController.getChatRoomManager().
-                    getChatRoomIdToName().values().contains(chatRoomNameInput)) {
-                programController.getChatRoomManager().createChatRoom(
-                        Arrays.asList(myUserId, friendUserId), chatRoomNameInput);
-                break;
-            }
-            else {
-                System.out.println("The name already exists. Please enter another one:");
-            }
-        }
-        // return newChatRoomId;
+    public void printEnterChatRoomName() {
+        System.out.println("Enter the name of the chat room:");
     }
+
+    public void printEnterUsernameOfPerson() {
+        System.out.println("Enter the username of the person you want to chat with:");
+    }
+
+    public void printCreateNameForChatRoom() {
+        System.out.println("Create a name for the chat room:");
+    }
+
+    public void printNameAlreadyExists() {
+        System.out.println("The name already exists. Please enter another one:");
+    }
+
+//    public void startChatRoomWithFriend() {
+//        System.out.println("Enter the username of the person you want to chat with:");
+//        String friendUsername = scanner.nextLine();
+//        UUID friendUserId = programController.getUsersManager().
+//                getIDFromUsername(friendUsername);
+//        System.out.println("Create a name for the chat room:");
+//        while (true) {
+//            String chatRoomNameInput = scanner.nextLine();
+//            if (!programController.getChatRoomManager().
+//                    getChatRoomIdToName().values().contains(chatRoomNameInput)) {
+//                programController.getChatRoomManager().createChatRoom(
+//                        Arrays.asList(myUserId, friendUserId), chatRoomNameInput);
+//                break;
+//            }
+//            else {
+//                System.out.println("The name already exists. Please enter another one:");
+//            }
+//        }
+//    }
 
     public void viewChatRooms() {
         System.out.println();
-        List<UUID> chatRoomIds = programController.getChatRoomManager().
-                fetchChatRoomsOfUser(myUserId);
+        List<UUID> chatRoomIds = messageScreenController.fetchChatRoomIds();
         for (UUID id : chatRoomIds) {
             System.out.println(programController.getChatRoomManager().
                     getChatRoomIdToName().get(id));
