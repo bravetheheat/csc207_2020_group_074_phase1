abb3df163d49b3a3c9dd3ad17c6750f9b2b5400b
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-12 17:33:00 +0800

    remove logic and add println() methods

--- src/main/presenters/ChatRoomScreen.java
+++ src/main/presenters/ChatRoomScreen.java
@@ -19,41 +19,70 @@ public class ChatRoomScreen extends Screen {
 
     @Override
     public void start() {
+//        System.out.println("Options: " + "1. View Messages" +
+//                "2. Start messaging" +
+//                "3. See List of Participants; " +
+//                "4. Rename this chat room" +
+//                "5. Delete this chat room" +
+//                "(Enter a number)");
+//        int input = scanner.nextInt();
+//        switch (input) {
+//            case 1:
+//                viewMessages(chatRoomId);
+//            case 2:
+//                String msg = scanner.nextLine();
+//                sendMessage(getFriendId(), msg);
+//            case 3:
+//                viewParticipants();
+//            case 4:
+//                System.out.println("Enter a new name for this chat room:");
+//                renameChatRoom();
+//            case 5:
+//                deleteChatRoom();
+//            default:
+//                System.out.println("Invalid Input!");
+//        }
+    }
+
+    public void chatRoomScreenStart() {
         System.out.println("Options: " + "1. View Messages" +
                 "2. Start messaging" +
                 "3. See List of Participants; " +
                 "4. Rename this chat room" +
                 "5. Delete this chat room" +
                 "(Enter a number)");
-        int input = scanner.nextInt();
-        switch (input) {
-            case 1:
-                viewMessages(chatRoomId);
-            case 2:
-                String msg = scanner.nextLine();
-                sendMessage(getFriendId(), msg);
-            case 3:
-                viewParticipants();
-            case 4:
-                System.out.println("Enter a new name for this chat room:");
-                renameChatRoom();
-            case 5:
-                deleteChatRoom();
-            default:
-                System.out.println("Invalid Input!");
-        }
     }
 
-    private UUID getFriendId() {
-        List<UUID> participantIds = programController.getChatRoomManager().
-                fetchChatRoom(chatRoomId).getParticipants();
-        return participantIds.get(1);
+    public void printEnterNewName() {
+        System.out.println("Enter a new name for this chat room:");
     }
 
-    public void viewMessages(UUID chatRoomId) {
+    public void printInvalidInput() {
+        System.out.println("Invalid Input!");
+    }
+
+    public void printBelowIsChatHistory () {
         System.out.println("Below is the chat history:");
-        programController.getChatRoomManager().fetchMessagesFromChatRoom(chatRoomId);
+    }
+
+    public void printLineBreak() {
         System.out.println("==============");
+    }
+
+//    private UUID getFriendId() {
+//        List<UUID> participantIds = programController.getChatRoomManager().
+//                fetchChatRoom(chatRoomId).getParticipants();
+//        return participantIds.get(1);
+//    }
+
+//    public void viewMessages(UUID chatRoomId) {
+//        System.out.println("Below is the chat history:");
+//        programController.getChatRoomManager().fetchMessagesFromChatRoom(chatRoomId);
+//        System.out.println("==============");
+//    }
+
+    public void printNameAlreadyExists() {
+        System.out.println("The name already exists. Please enter another one:");
     }
 
     public void viewParticipants() {
@@ -64,26 +93,26 @@ public class ChatRoomScreen extends Screen {
         }
     }
 
-    public void renameChatRoom() {
-        while (true) {
-            String name = scanner.nextLine();
-            if (!programController.getChatRoomManager().
-                    getChatRoomIdToName().values().contains(name)) {
-                programController.getChatRoomManager().
-                        fetchChatRoom(chatRoomId).setName(name);
-                break;
-            }
-            else {
-                System.out.println("The name already exists. Please enter another one:");
-            }
-        }
-    }
+//    public void renameChatRoom() {
+//        while (true) {
+//            String name = scanner.nextLine();
+//            if (!programController.getChatRoomManager().
+//                    getChatRoomIdToName().values().contains(name)) {
+//                programController.getChatRoomManager().
+//                        fetchChatRoom(chatRoomId).setName(name);
+//                break;
+//            }
+//            else {
+//                System.out.println("The name already exists. Please enter another one:");
+//            }
+//        }
+//    }
 
-    public void deleteChatRoom() {
-        programController.getChatRoomManager().deleteChatRoom(chatRoomId);
-    }
+//    public void deleteChatRoom() {
+//        programController.getChatRoomManager().deleteChatRoom(chatRoomId);
+//    }
 
-    public void sendMessage(UUID friendId, String message) {
-        programController.getMessageManager().createMessage(message, friendId);
-    }
+//    public void sendMessage(UUID friendId, String message) {
+//        programController.getMessageManager().createMessage(message, friendId);
+//    }
 }
