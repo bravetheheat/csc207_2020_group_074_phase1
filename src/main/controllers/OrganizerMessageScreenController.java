4f40b20921a5571669a081bae316fd1b1ee03e30
stevenY <ysc.yuan@mail.utoronto.ca>
2020-11-13 21:52:40 +0800

    add javadoc

--- src/main/controllers/OrganizerMessageScreenController.java
+++ src/main/controllers/OrganizerMessageScreenController.java
@@ -1,11 +1,23 @@
 package main.controllers;
 
+/**
+ * @author Steven Yuan
+ * @version 1.0
+ * @since 2020-11-13
+ */
 public class OrganizerMessageScreenController extends MessageScreenController {
 
+    /**
+     * Instantiate an <code>OrganizerMessageScreenController</code> object
+     * @param programController the program controller
+     */
     public OrganizerMessageScreenController(ProgramController programController) {
         super(programController);
     }
 
+    /**
+     * Send a message to all participants of an event.
+     */
     public void sendMessageToAll() {
 
     }
