2efc016faf41f8eb1a40493c030ad8d7414807bf
hiraethwly <wlyleyi@gmail.com>
2020-11-13 22:44:42 -0500

    change addUser to createSpeaker

--- src/main/presenters/UserManagementScreen.java
+++ src/main/presenters/UserManagementScreen.java
@@ -11,22 +11,16 @@ public class UserManagementScreen{
 
     public void promptCommand(){
         System.out.println("What would you like to modify, choose one option (number only) from below:");
-        System.out.println("1. Add user");
-        System.out.println("2. Remove user");
-        System.out.println("3. Display user list");
-        System.out.println("4. Exit the screen");
+        System.out.println("1. Add Speaker");
+        System.out.println("2. Display user list");
+        System.out.println("3. Exit the screen");
     }
-    public void promptAddUser(){
-        System.out.println("You are adding a user, please enter the following information line by line:");
+    public void promptCreateSpeaker(){
+        System.out.println("You are adding a speaker, please enter the following information line by line:");
         System.out.println("Line 1 -- Enter user name: ");
         System.out.println("Line 2 -- Enter user password: ");
-        System.out.println("Line 3 -- Enter user type: ");
     }
 
-    public void promptDeleteUser(){
-        System.out.println("You are deleting a user, please enter the following information line by line:");
-        System.out.println("Enter user id: ");
-    }
 
     public void listUser(String userList) {
         System.out.println(userList);
@@ -34,18 +28,10 @@ public class UserManagementScreen{
 
     public void printInvalidInput() { System.out.println("Invalid input. Try again.");}
 
-    public void printValidAdding(){ System.out.println("Successfully added user!"); }
+    public void printValidAdding(){ System.out.println("Successfully added speaker!"); }
 
     public void printInvalidAdding(){
-        System.out.println("Fail to add user! Please try again.");
-    }
-
-    public void printValidRemove(){
-        System.out.println("Successfully removed user!");
-    }
-
-    public void printInvalidRemove(){
-        System.out.println("Fail to remove user! Please try again.");
+        System.out.println("Fail to add speaker! Please try again.");
     }
 
 }
