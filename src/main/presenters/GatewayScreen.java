6acc96a44c5d9746de2ccc9e56d3f996df14d025
Yile Xie <yvette.xie626@gmail.com>
2020-11-19 14:19:25 -0500

    Add Javadoc for GatewayScreen, MessageDetailScreenController, and OrganizerMessageScreen

--- src/main/presenters/GatewayScreen.java
+++ src/main/presenters/GatewayScreen.java
@@ -7,11 +7,17 @@ package main.presenters;
  */
 public class GatewayScreen {
 
+    /**
+     * Print out the welcome message
+     */
     public void welcomeMessage() {
         System.out.println("Gateway Screen");
         System.out.println();
     }
 
+    /**
+     * Print out a list of options
+     */
     public void optionsPrompt() {
         System.out.println("Please choose from one of the following options:");
         System.out.println("0. Return to previous screen.");
@@ -23,35 +29,56 @@ public class GatewayScreen {
         System.out.println("6. Save inboxes");
     }
 
+    /**
+     * Notify that the program is saving the user data
+     */
     public void saveUsers() {
         System.out.println("Saving users...");
         System.out.println();
     }
 
+    /**
+     * Notify that the program is saving the room data
+     */
     public void saveRooms() {
         System.out.println("Saving rooms...");
         System.out.println();
     }
 
+    /**
+     * Notify that the program is saving the event data
+     */
     public void saveEvents() {
         System.out.println("Saving events...");
         System.out.println();
     }
 
+    /**
+     * Notify that the program is saving the message data
+     */
     public void saveMessages() {
         System.out.println("Saving messages...");
         System.out.println();
     }
 
+    /**
+     * Notify that the program is saving the inbox data
+     */
     public void saveInboxes() {
         System.out.println("Saving inboxes...");
         System.out.println();
     }
 
+    /**
+     * Notify that the operation is successful
+     */
     public void success() {
         System.out.println("Success!");
     }
 
+    /**
+     * Tell the user that the option they entered is invalid
+     */
     public void invalidOption() {
         System.out.println("Invalid option. Please try again.");
     }
--- src/main/screencontrollers/MessageDetailScreenController.java
+++ src/main/screencontrollers/MessageDetailScreenController.java
@@ -5,30 +5,47 @@ import main.controllers.ProgramController;
 import main.presenters.MessageDetailScreen;
 
 /**
- * @
+ * The MessageDetailScreenController determines what the user would see when they choose to open a message
+ *
+ * @author Yile Xie
  */
 public class MessageDetailScreenController extends ScreenController {
     String messageId;
     MessageDetailScreen presenter;
 
+    /**
+     * A ProgramController and a messageId (as a string) are required to instantiate a MessageDetailScreenController
+     * @param programController of this program
+     * @param messageId of the message this user selects
+     */
     public MessageDetailScreenController(ProgramController programController, String messageId) {
         super(programController);
         this.messageId = messageId;
         this.presenter = new MessageDetailScreen();
     }
 
+    /**
+     * Display the details of the message and prompt the user to return
+     */
     public void start() {
         this.printDetails(messageId);
         this.returnPrompt();
         this.end();
     }
 
+    /**
+     * Print out the details of the message with given ID, including the text, the sender, and the time this message
+     * was sent
+     */
     public void printDetails(String messageId) {
         InboxController inboxController = new InboxController(this.programController);
         String detail = inboxController.getMessageString(messageId);
         this.presenter.printDetails(detail);
     }
 
+    /**
+     * Prompt the user to enter 0 if they wish to return to their inbox screen
+     */
     public void returnPrompt() {
         this.presenter.returnPrompt();
         String input = this.scanner.nextLine();
