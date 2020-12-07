2b02dafabb4c10ea70851aa67b1071037dce6e72
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-07 20:22:11 +0800

    add GUI entry point

--- src/main/controllers/Main.java
+++ src/main/controllers/Main.java
@@ -1,9 +1,14 @@
 package main.controllers;
 
+import main.gui.LandingUI;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
         ProgramController program = new ProgramController();
-        program.start();
+//        program.start();
+        SwingUtilities.invokeLater(() -> new LandingUI(program));
     }
 }
