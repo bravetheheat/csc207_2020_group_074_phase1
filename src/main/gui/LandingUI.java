e56b7103980aaff8c4a437ea0107b37ad7ee6f5d
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 17:00:40 +0800

    minor changes

--- src/main/gui/LandingUI.java
+++ src/main/gui/LandingUI.java
@@ -9,30 +9,21 @@ import javax.swing.*;
 public class LandingUI extends JFrame {
     private ProgramController programController;
     private JPanel mainPanel;
-    private JTextField welcomeText;
-    private JButton exitButton;
     private JButton logInButton;
     private JButton registerButton;
+    private JLabel titleLabel;
 
 
     public LandingUI(ProgramController programController) {
         this.programController = programController;
+        programController.startUI();
 
         this.setTitle("Program X");
         this.setSize(600, 500);
-//        this.add(mainPanel);
-//        this.add(welcomeText);
-//        this.add(exitButton);
-//        this.add(logInButton);
-//        this.add(registerButton);
         this.setContentPane(this.mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-
-        exitButton.addActionListener(e -> {
-            // programController.setNextScreen(null);
-        });
 
         logInButton.addActionListener(e -> {
             new LoginUI(programController);
