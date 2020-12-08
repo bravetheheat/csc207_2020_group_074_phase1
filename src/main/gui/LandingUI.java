499c552c9aed7e0a3905b69b3158bb10352e8c61
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 21:40:39 +0800

    remove previous codes and comments; add javadoc

--- src/main/gui/LandingUI.java
+++ src/main/gui/LandingUI.java
@@ -1,9 +1,16 @@
 package main.gui;
 
 import main.controllers.ProgramController;
+import main.guilisteners.LoginButtonListener;
+import main.guilisteners.RegisterButtonListener;
 
 import javax.swing.*;
 
+/**
+ * The main/initial frame of the application.
+ *
+ * @author Steven Yuan
+ */
 @SuppressWarnings("FieldCanBeLocal")
 
 public class LandingUI extends JFrame {
@@ -12,31 +19,37 @@ public class LandingUI extends JFrame {
     private JButton logInButton;
     private JButton registerButton;
     private JLabel titleLabel;
+    private LoginButtonListener loginButtonListener;
+    private RegisterButtonListener registerButtonListener;
 
-
-    public LandingUI(ProgramController programController) {
-        this.programController = programController;
-        programController.startUI();
-
+    public LandingUI() {
         this.setTitle("Program X");
         this.setSize(600, 500);
         this.setContentPane(this.mainPanel);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
-        logInButton.addActionListener(e -> {
-            programController.saveForNext();
-            new LoginUI(programController);
-            dispose();
-        });
+        logInButton.addActionListener(e -> notifyListenerOnLoginButtonClicked());
 
-        registerButton.addActionListener(e -> {
-            programController.saveForNext();
-            new RegisterUI(programController);
-            dispose();
-        });
+        registerButton.addActionListener(e -> notifyListenerOnRegisterButtonClicked());
 
+    }
+
+    public void addLoginButtonListener(LoginButtonListener listener) {
+        loginButtonListener = listener;
+    }
+
+    public void addRegisterButtonLister(RegisterButtonListener listener) {
+        registerButtonListener = listener;
+    }
+
+    public void notifyListenerOnLoginButtonClicked() {
+        loginButtonListener.onLoginButtonClicked();
+    }
+
+    public void notifyListenerOnRegisterButtonClicked() {
+        registerButtonListener.onRegisterButtonClicked();
     }
 
 }
