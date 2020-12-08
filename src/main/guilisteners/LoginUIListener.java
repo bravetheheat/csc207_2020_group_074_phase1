c8ab1a014691cec1bbcd2c4273261d1723b3d3ef
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 21:02:19 +0800

    add listener interface for the presenter to implement

---
+++ src/main/guilisteners/LoginUIListener.java
@@ -0,0 +1,11 @@
+package main.guilisteners;
+
+/**
+ * The LoginUIListener handles login functionality of the program
+ *
+ * @author Steven Yuan
+ * @version 1.0
+ */
+public interface LoginUIListener {
+    void onLoginButtonClicked();
+}
