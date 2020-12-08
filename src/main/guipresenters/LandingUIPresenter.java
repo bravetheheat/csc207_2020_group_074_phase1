4288421f5ad9f734cbbc180175905d316d248e83
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 21:09:52 +0800

    logic for landing UI and button actions

---
+++ src/main/guipresenters/LandingUIPresenter.java
@@ -0,0 +1,41 @@
+package main.guipresenters;
+
+import main.controllers.ProgramController;
+import main.gui.LandingUI;
+import main.gui.LoginUI;
+import main.gui.RegisterUI;
+import main.guilisteners.LoginButtonListener;
+import main.guilisteners.RegisterButtonListener;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class LandingUIPresenter implements LoginButtonListener, RegisterButtonListener {
+
+    private LandingUI landingUI;
+    private ProgramController programController;
+    private LoginUI loginUI;
+    private RegisterUI registerUI;
+
+    public LandingUIPresenter(LandingUI landingUI, ProgramController programController) {
+        this.landingUI = landingUI;
+        this.programController = programController;
+        programController.startUI();
+        landingUI.addLoginButtonListener(this);
+        landingUI.addRegisterButtonLister(this);
+    }
+
+    @Override
+    public void onLoginButtonClicked() {
+        programController.saveForNext();
+        loginUI = new LoginUI();
+        new LoginUIPresenter(loginUI, programController);
+        landingUI.dispose();
+    }
+
+    @Override
+    public void onRegisterButtonClicked() {
+        programController.saveForNext();
+        registerUI = new RegisterUI();
+        new RegisterUIPresenter(registerUI, programController);
+        landingUI.dispose();
+    }
+}
