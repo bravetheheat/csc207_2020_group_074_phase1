1e72ac45d83e20266174ee38463674caf4611525
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 21:02:58 +0800

    add listener variables and move logic to the presenter

--- src/main/gui/LoginUI.java
+++ src/main/gui/LoginUI.java
@@ -1,7 +1,7 @@
 package main.gui;
 
-import main.controllers.AuthController;
-import main.controllers.ProgramController;
+import main.guilisteners.BackButtonListener;
+import main.guilisteners.LoginUIListener;
 
 import javax.swing.*;
 
@@ -9,63 +9,88 @@ import javax.swing.*;
 
 public class LoginUI extends JFrame {
 
-    private ProgramController programController;
+//    private ProgramController programController;
     private JPanel loginPanel;
     private JButton backButton;
     private JTextField emailTextField;
     private JButton logInButton;
     private JPasswordField passwordField;
     private JLabel loginTitleLabel;
-    private final AuthController authController;
+//    private final AuthController authController;
+    private LoginUIListener loginUIListener;
+    private BackButtonListener backButtonListener;
 
-    public LoginUI(ProgramController programController) {
-        this.programController = programController;
+    public LoginUI() {
 
         this.setSize(600, 500);
         this.setContentPane(loginPanel);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
-        this.authController = programController.getAuthController();
 
         logInButton.addActionListener(e -> {
-            String username = emailTextField.getText();
-            String password = String.valueOf((passwordField.getPassword()));
-            if (authController.login(username, password)) {
-                String userType = authController.getUserType();
-                switch(userType){
-                    case "Attendee":
-                        System.out.println("go to attendee");
-                        programController.saveForNext();
-                        new AttendeeMainUI(programController);
-                        dispose();
-                        break;
-                    case "Organizer":
-                        System.out.println("go to organizer");
-                        programController.saveForNext();
-                        new OrganizerMainUI(programController);
-                        dispose();
-                        break;
-                    case "Speaker":
-                        System.out.println("go to speaker");
-                        programController.saveForNext();
-                        new SpeakerMainUI(programController);
-                        dispose();
-                        break;
-                    default:
-                        programController.saveForNext();
-                        new RegisterMessageErrorUI(programController);
-                        dispose();
-                }
-            }
+//            String username = emailTextField.getText();
+//            String password = String.valueOf((passwordField.getPassword()));
+//            if (authController.login(username, password)) {
+//                String userType = authController.getUserType();
+//                switch(userType){
+//                    case "Attendee":
+//                        System.out.println("go to attendee");
+//                        programController.saveForNext();
+//                        new AttendeeMainUI(programController);
+//                        dispose();
+//                        break;
+//                    case "Organizer":
+//                        System.out.println("go to organizer");
+//                        programController.saveForNext();
+//                        new OrganizerMainUI(programController);
+//                        dispose();
+//                        break;
+//                    case "Speaker":
+//                        System.out.println("go to speaker");
+//                        programController.saveForNext();
+//                        new SpeakerMainUI(programController);
+//                        dispose();
+//                        break;
+//                    default:
+//                        programController.saveForNext();
+//                        new RegisterMessageErrorUI(programController);
+//                        dispose();
+//                }
+//            }
+            notifyListenerOnLoginButtonClicked();
         });
-        // need to find out how to display wrong information message
 
         backButton.addActionListener(e -> {
-            programController.saveForNext();
-            new LandingUI(programController);
-            dispose();
+//            programController.saveForNext();
+//            new LandingUI(programController);
+//            dispose();
+            notifyListenerOnBackButtonClicked();
         });
+    }
+
+    public void addLoginUIListener(LoginUIListener listener) {
+        loginUIListener = listener;
+    }
+
+    public void addBackButtonListener(BackButtonListener listener) {
+        backButtonListener = listener;
+    }
+
+    public void notifyListenerOnLoginButtonClicked() {
+        loginUIListener.onLoginButtonClicked();
+    }
+
+    public void notifyListenerOnBackButtonClicked() {
+        backButtonListener.onBackButtonClicked();
+    }
+
+    public String getUserName() {
+        return emailTextField.getText();
+    }
+
+    public String getPwd() {
+        return String.valueOf((passwordField.getPassword()));
     }
 }
