f946cc29a5aa8eede342d11f4e9fd04b060a5370
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-08 21:41:35 +0800

    use model-view-presenter structure

--- src/main/controllers/Main.java
+++ src/main/controllers/Main.java
@@ -1,14 +1,21 @@
 package main.controllers;
 
-import main.gui.LandingUI;
+import main.gui.*;
+import main.guipresenters.LandingUIPresenter;
 
 import javax.swing.*;
 
 public class Main {
 
+    public Main() {
+        // model-view-presenter
+        LandingUI landingUI = new LandingUI(); // view
+        ProgramController program = new ProgramController(); // model
+        new LandingUIPresenter(landingUI, program); // presenter
+    }
+
     public static void main(String[] args) {
-        ProgramController program = new ProgramController();
 //        program.start();
-        SwingUtilities.invokeLater(() -> new LandingUI(program));
+        SwingUtilities.invokeLater(Main::new);
     }
 }
