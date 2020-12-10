c8e4a248dde76700f5ba43bd9cf9ee28b35c2cc0
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-10 23:04:20 +0800

    add javadoc

--- src/main/gui_interface/ILoginUI.java
+++ src/main/gui_interface/ILoginUI.java
@@ -4,18 +4,50 @@ import main.gui.LandingUI;
 import main.guilisteners.BackButtonListener;
 import main.guilisteners.LoginUIListener;
 
+/**
+ * The interface for <code>LoginUI</code>
+ *
+ * @author Steven Yuan
+ */
 public interface ILoginUI extends IView {
+
+    /**
+     * Add the class that implements <code>LoginUIListener</code>
+     * @param listener listens to actions performed on the login button and switch screen
+     */
     void addLoginUIListener(LoginUIListener listener);
 
+    /**
+     * Add the class that implements <code>BackButtonListener</code>
+     * @param listener listens to actions performed on the back button and switch screen
+     */
     void addBackButtonListener(BackButtonListener listener);
 
+    /**
+     * Call the presenter method to update accordingly
+     */
     void notifyListenerOnLoginButtonClicked();
 
+    /**
+     * Call the presenter method to update accordingly
+     */
     void notifyListenerOnBackButtonClicked();
 
+    /**
+     * Get the username from the current user
+     * @return username
+     */
     String getUserName();
 
+    /**
+     * Get the password from the current user
+     * @return password
+     */
     String getPwd();
 
+    /**
+     * Go back to the main screen
+     * @return <code>LandingUI</code>
+     */
     LandingUI goToLandingUI();
 }
