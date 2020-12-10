42928b57a3302fdcf7af47b91d8d7228804d9133
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-10 20:22:16 +0800

    add landing UI interface

---
+++ src/main/gui_interface/ILandingUI.java
@@ -0,0 +1,20 @@
+package main.gui_interface;
+
+import main.guilisteners.LoginButtonListener;
+import main.guilisteners.RegisterButtonListener;
+
+/**
+ * The <code>LandingUIPresenter</code> will depend on this interface which is
+ * implemented by <code>LandingUI</code>
+ *
+ * @author Steven Yuan
+ */
+public interface ILandingUI extends IView {
+    void addLoginButtonListener(LoginButtonListener listener);
+
+    void addRegisterButtonLister(RegisterButtonListener listener);
+
+    void notifyListenerOnLoginButtonClicked();
+
+    void notifyListenerOnRegisterButtonClicked();
+}
