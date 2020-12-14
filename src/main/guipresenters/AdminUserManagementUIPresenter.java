07e8426ee67bff59392a9b3a5baed5e936a72388
stevenY <ysc.yuan@mail.utoronto.ca>
2020-12-14 12:12:26 +0800

    Merge branch 'main' of https://github.com/bravetheheat/csc207_2020_group_074_phase1 into steven
    # Conflicts:
    #	src/store/app.db
    
    merge with luke

---
+++ src/main/guipresenters/AdminUserManagementUIPresenter.java
@@ -0,0 +1,45 @@
+package main.guipresenters;
+
+import main.controllers.AuthController;
+import main.controllers.ProgramController;
+import main.gui_interface.IAdminUserManagementUI;
+import main.guilisteners.BackButtonListener;
+import main.guilisteners.DeleteUserButtonListener;
+
+public class AdminUserManagementUIPresenter extends UserManagementUIPresenter
+        implements BackButtonListener, DeleteUserButtonListener {
+
+    private ProgramController programController;
+    private AuthController authController;
+    private IAdminUserManagementUI iAdminUserManagementUI;
+
+    public AdminUserManagementUIPresenter(IAdminUserManagementUI adminUserManagementUI, ProgramController programController) {
+        super(adminUserManagementUI, programController);
+        this.programController = programController;
+        this.authController = this.programController.getAuthController();
+        this.iAdminUserManagementUI = adminUserManagementUI;
+        iAdminUserManagementUI.addDeleteUserButtonListener(this);
+    }
+
+    @Override
+    public void onBackButtonClicked() {
+        //TODO: complete the method after AdminMainUI is created
+    }
+
+    @Override
+    public void onDeleteUserButtonClicked() {
+        String userToBeDeleted = iAdminUserManagementUI.getDeletedUser();
+        if (userToBeDeleted.equals("")) {
+            iAdminUserManagementUI.deleteUserError();
+            return;
+        }
+        if (programController.getUsersManager().removeUserbyUsername(userToBeDeleted)) {
+            iAdminUserManagementUI.deleteUserSuccessful();
+        }
+        else {
+            iAdminUserManagementUI.deleteUserError();
+        }
+    }
+
+
+}
